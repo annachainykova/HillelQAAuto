5cea8ffdce33ee08544b5a21daf98bae51005c1c
Anna <a.chainikova@gmail.com>
2018-05-05 11:29:39 +0300

    improving RandomNumber class

--- src/main/java/RandomNumber.java
+++ src/main/java/RandomNumber.java
@@ -3,54 +3,48 @@ import java.util.Scanner;
 
 public class RandomNumber {
     public static void main(String[] args) {
+        int range = 10; // range of random number between 0 and this number (exclusive)
+        int randomNumber = randomNumber(range);
+        boolean result;
+        do {
+            int enteredNumber = enterNumber(range);
+            result = isSameNumber(randomNumber, enteredNumber);
+            if (result) {
+                System.out.println("Congratulations! You guessed a number");
+            } else if (enteredNumber > randomNumber) {
+                System.out.println("Your number is bigger");
+            } else {
+                System.out.println("Your number is smaller");
+            }
+        } while (!result);
 
-//        System.out.println(isSameNumber());
-//        if(isSameNumber()){
-//            System.out.println("You guessed number");
-//        } else {
-//            enterNumber();
-//        }
-
-
-        Random rand = new Random();
-        int x = rand.nextInt(9);
-        System.out.println(x);
-        Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Enter a number between 0 and 9: ");
-        String m = reader.nextLine();
-        if(m.matches("\\d") ) {
-        int n = Integer.parseInt(m);
-                if (n == x) {
-                    System.out.println("you win");
-                } else if (n < x) {
-                    System.out.println("Your number is smaller, try again");
-                } else {
-                    System.out.println("Your number is bigger, try again");
-                }
-        } else {
-            System.out.println("Please enter valid number");
-        }
 
     }
 
+    public static int randomNumber(int range) {
+        Random rand = new Random();
+        int x = rand.nextInt(range);
+        return x;
+    }
 
-//    public static int RandomNumber() {
-//        Random rand = new Random();
-//        int x = rand.nextInt(9);
-//        System.out.println(x);
-//        return x;
-//    }
+    public static int enterNumber(int range) {
+        Scanner sc = new Scanner(System.in);
+        String s;
+        boolean isNumber;
+        do {
+            System.out.println("Please enter a number between 0 and : " + (range - 1));
+            s = sc.nextLine();
+            if (!(isNumber = s.matches("\\d+"))) {
+                System.out.println("Not a valid number");
+            }
+        } while (!isNumber);
+        return Integer.parseInt(s);
 
-//    public static int enterNumber() {
-//        Scanner reader = new Scanner(System.in);  // Reading from System.in
-//        System.out.println("Enter a number: ");
-//        int n = reader.nextInt();
-//        return n;
-//
-//    }
+    }
 
-//    public static boolean isSameNumber() {
-//        return RandomNumber() == enterNumber();
-//    }
+    public static boolean isSameNumber(int randomNumber, int enteredNumber) {
+        return randomNumber == enteredNumber;
+    }
+
 
 }
