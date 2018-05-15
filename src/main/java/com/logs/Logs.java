de4c8909c77397de9556a8a87133a7ac6d21d624
Анна Чайникова <a.chainikova@gmail.com>
2018-05-15 21:27:57 +0300

    divide string by regex

--- src/main/java/com/logs/Logs.java
+++ src/main/java/com/logs/Logs.java
@@ -10,19 +10,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Logs {
     public static void main(String[] args) throws IOException {
             //logsToLogs("LogsToTest");
-        parse("Feb  3 16:18:28 gw2-nc-biva transaction1: Asset id: 5153");
+            fromFileOnlyLogsWithTIme(logsToLogs("LogsToTest"));
+        //regex();
 
     }
 
-    public static void parse (String log) {
-
-    }
 
 
 
@@ -59,9 +59,7 @@ public class Logs {
 
 
 
-
-
-        public static void logsToLogs (String path) throws IOException { File folder = new File(path);
+        public static String logsToLogs (String path) throws IOException { File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
 
         List<String> newLines = new ArrayList();
@@ -77,16 +75,46 @@ public class Logs {
             List<String> lines = Files.readAllLines(Paths.get(fileName));
 
             for (int i = 0; i < lines.size(); i++) {
-                if (lines.get(i).contains("Asset id:")) {
+                Pattern p = Pattern.compile("(\\s){2}(\\w)*(-)*(\\w)+-(\\d)+");
+                Matcher m = p.matcher(lines.get(i));
+                if (m.find()) {
                     newLines.add(lines.get(i));
-                    newLines.add(lines.get(i + 1));
-                    newLines.add(lines.get(i + 2));
                 }
             }
 
             Files.write(fileToWrite, newLines);
 
         }
+        return pathofNew;
+    }
+
+    public static void fromFileOnlyLogsWithTIme(String pathOfNew) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(pathOfNew));
+        for (String line : lines) {
+            String[] parts = line.split(":  ");
+            String part1 = parts[0];
+            String part2 = parts[1];
+
+            Pattern p = Pattern.compile("([a-zA-z]*\\s*\\d\\s\\d*:\\d*:\\d*)(\\s*(\\w*-)*\\w*\\s*\\w*)");
+            Matcher m = p.matcher(part1);
+            m.find();
+           String timeOnly = m.group(1);
+
+            System.out.println(timeOnly);
+            System.out.println(part2);
+        }
+    }
+
+
+    public static void regex() {
+        String str = "Feb  4 00:07:18 gw1-ca-ca1 transaction0";
+        Pattern p = Pattern.compile("([a-zA-z]*\\s*\\d\\s\\d*:\\d*:\\d*)(\\s*(\\w*-)*\\w*\\s*\\w*)");
+        Matcher m = p.matcher("Feb  4 00:07:18 gw1-ca-ca1 transaction0");
+        System.out.println(str);
+        System.out.println(m.find());
+        String re = m.group(1);
+        System.out.println(re);
+
     }
 
 
