47803a8c665969926a8609522a32b277caa0cd92
Anna <a.chainikova@gmail.com>
2018-05-18 13:22:28 +0300

    added some improvements

--- src/main/java/com/logs/LogsNew.java
+++ src/main/java/com/logs/LogsNew.java
@@ -1,10 +1,10 @@
 package com.logs;
 
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -18,22 +18,20 @@ import java.util.regex.Pattern;
 
 
 // To Do
-// Fix 2WeeksDate
 // Improve all methods
 // Add comments
-// delete useless methods
-
-
 
 
 
 public class LogsNew {
     public static void main(String[] args) throws IOException, ParseException {
-
-        //writeMapIntoFile(transactionsOfTime(getOnlyLinesWithTransactions(getListOfFilesInFolder("E:\\Anna\\Logs"))));
-        //date();
-        //howManyTimesPerDay(transactionsOfTime(getOnlyLinesWithTransactions(getListOfFilesInFolder("E:\\Anna\\Logs"))));
-        writeMapIntoFile(howManyTimesPerWeek(uniteTransactionForTwoWeeks(transactionsOfTime(getOnlyLinesWithTransactions(getListOfFilesInFolder("E:\\Anna\\Logs"))))));
+        writeMapIntoFile(
+                howManyTimesPerPeriode(
+                        uniteTransactionsOfSpecificPeriod(
+                                combineTransactionsOfDay(
+                                        getOnlyLinesWithTransactions(
+                                                getAllLinesFromFiles(
+                                                    getListOfFilesInFolder("E:\\Anna\\Logs")))), 14)));
     }
 
     /**
@@ -50,58 +48,75 @@ public class LogsNew {
 
     /**
      *
-     * @param listOfFiles Array of Files, that will be read and checked for specific Strings (march pattern)
+     * @param listOfFiles Array of Files
+     * @return ArrayList which consists of all lines from each files
+     * @throws IOException
+     */
+    public static List <String> getAllLinesFromFiles (File[] listOfFiles) throws IOException {
+        List <String> linesOfFiles = new ArrayList();
+
+        for (File file : listOfFiles) {
+            List<String> linesOfFile = Files.readAllLines(Paths.get(file.getPath()));
+            for (String line : linesOfFile) {
+                    linesOfFiles.add(line);
+            }
+        }
+        return linesOfFiles;
+    }
+
+
+
+    /**
+     *
+     * @param allLinesFromFiles ArrayList of all lines
      * @return List of String, each String must 'match' the specific pattern
      * @throws IOException
      */
-    public static List<String> getOnlyLinesWithTransactions (File[] listOfFiles) throws IOException {
+    public static List <String> getOnlyLinesWithTransactions (List <String> allLinesFromFiles)  {
         List <String> linesWithTransactionsID = new ArrayList();
-
-        for (File file : listOfFiles) {
-            List<String> lines = Files.readAllLines(Paths.get(file.getPath()));
-
-            for (String line : lines) {
-                Pattern p = Pattern.compile("(\\s){2}(\\w)*(-)*(\\w)+-(\\d)+");
+            for (String line : allLinesFromFiles) {
+                Pattern p = Pattern.compile("(\\s){2}(\\w)*(-)*(\\w)+-(\\d)+"); //this pattern checks sequence that must match TranscationsID
                 Matcher m = p.matcher(line);
                 if (m.find()) {
                     linesWithTransactionsID.add(line);
                 }
             }
-        }
         return linesWithTransactionsID;
     }
+
+
 
     /**
      *
      * @param linesWithTransactionsID
-     * Divide lines two DateTime, ServerName and Transactions IDs and save DateTime and Transactions IDs into treeMap
-     * @return treeMap which contains time as a key and all transactions for this time as a value
+     * Divide lines to DateTime, ServerName and Transactions IDs and save DateTime and Transactions IDs into treeMap
+     * @return treeMap which contains time as a key and all transactions for this time(each day) as a value
      */
-    public static TreeMap<LocalDate, String> transactionsOfTime (List<String> linesWithTransactionsID) throws ParseException {
+    public static TreeMap <LocalDate, String> combineTransactionsOfDay (List <String> linesWithTransactionsID) throws ParseException {
 
-        HashMap <LocalDate, String> timeAndTransactions = new HashMap<>();
+        TreeMap <LocalDate, String> timeAndTransactions = new TreeMap<>();
+
         for (String line : linesWithTransactionsID) {
 
             // Spliting each line of list by regex for getting two parts:
             // part1 = DateTime with ServerName;
             // part2 = transactionsIDs only
             String[] parts = line.split(":  ");
-            String timeWithServername = parts[0];
+            String timeWithServerName = parts[0];
             String transactionsIDs = parts[1];
 
+
             Pattern p = Pattern.compile("([a-zA-z]{3}\\s{2}\\d{1,2})(\\s{1}\\d{2}:\\d{2}:\\d{2})(\\s*(\\w*-)*\\w*\\s*\\w*)"); //regex to check DateTime and ServerName
-            Matcher m = p.matcher(timeWithServername);
+            Matcher m = p.matcher(timeWithServerName);
             m.find();
 
             String date = m.group(1); // saving only Date
-            String datePlusYear = date + " 2018";
-            String dateTime = m.group(1) + " " + m.group(2);
+            String datePlusYear = date + " 2018"; //adding year, must be to work with LocalDate
+
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM  d yyyy");
             LocalDate dateEvery = LocalDate.parse(datePlusYear, formatter);
 
-            String serverName = m.group(3);
-
-            //adding values to hashmap with unique key, concat value if needed
+            //adding values to treemap with unique key, concat value if needed
             if(timeAndTransactions.containsKey(dateEvery)) {
                 transactionsIDs = timeAndTransactions.get(dateEvery) + "," + transactionsIDs;
             }
@@ -110,127 +125,85 @@ public class LogsNew {
         return new TreeMap<>(timeAndTransactions);
     }
 
-    /**
-     * Write Map into File with specific
-     * @param timeAndTransactions
-     * @throws IOException
-     */
-    public static void writeMapIntoFile(TreeMap <String, TreeMap <String, Integer>> timeAndTransactions) throws FileNotFoundException {
-        String currentTime = LocalDateTime.now().toString().replace(':', '-'); //to make specific name of File
 
-        PrintWriter out = new PrintWriter("logsUpdated" + currentTime + ".txt");
+    public static TreeMap <String, String> uniteTransactionsOfSpecificPeriod(TreeMap <LocalDate, String> timeAndTransactions, int periodInDays) {
+        LocalDate firstDayOfAllLogs = timeAndTransactions.firstKey();
+        LocalDate firstDayOfPeriod = firstDayOfAllLogs;
+        LocalDate lastDateOfPeriod = firstDayOfAllLogs.plusDays(periodInDays);
+        TreeMap <String, String> transactionsForSpecificPeriod= new TreeMap<>();
 
-        Set<Map.Entry<String, TreeMap<String, Integer>>> hashSet=timeAndTransactions.entrySet();
-        for(Map.Entry<String, TreeMap<String, Integer>> entry:hashSet ) {
-
-            out.println(entry.getKey() + " :");
-            out.println(entry.getValue());
-        }
-    }
-
-    public static  TreeMap<String, String> uniteTransactionForTwoWeeks(TreeMap<LocalDate, String> timeAndTransactions) {
-        LocalDate firstDayEver = timeAndTransactions.firstKey();
-        TreeMap <String, String> weeksAndTransactions= new TreeMap<>();
-        String myString = "";
+        String logsOfPeriod = "";
         List<String> allString = new ArrayList<>();
-        LocalDate currentDate = firstDayEver;
-        while(currentDate.isBefore(timeAndTransactions.lastKey().plusDays(1))) {
-            while(currentDate.isBefore(firstDayEver.plusWeeks(2))) {
-            if(timeAndTransactions.containsKey(currentDate)) {
-                myString = myString + "," + (timeAndTransactions.get(currentDate));
+        LocalDate currentDate = firstDayOfAllLogs;
 
-            }
+        while(currentDate.isBefore(timeAndTransactions.lastKey().plusDays(1))) {
+            while(currentDate.isBefore(lastDateOfPeriod) && currentDate.isBefore(timeAndTransactions.lastKey().plusDays(1))) {
+                if(timeAndTransactions.containsKey(currentDate)) {
+                    if (logsOfPeriod.length() == 0) {
+                        logsOfPeriod = (timeAndTransactions.get(currentDate));
+                    } else {
+                        logsOfPeriod = logsOfPeriod + "," + timeAndTransactions.get(currentDate);
+                    }
+                }
                 currentDate = currentDate.plusDays(1);
             }
-            weeksAndTransactions.put(firstDayEver + " - " + firstDayEver.plusWeeks(2).minusDays(1), myString);
-            allString.add(myString);
-            myString = "";
-            firstDayEver = firstDayEver.plusWeeks(2);
+            transactionsForSpecificPeriod.put(firstDayOfPeriod + " - " + lastDateOfPeriod.minusDays(1), logsOfPeriod);
+            allString.add(logsOfPeriod);
+            logsOfPeriod = "";
+            firstDayOfPeriod = firstDayOfPeriod.plusDays(periodInDays);
+            lastDateOfPeriod = lastDateOfPeriod.plusDays(periodInDays);
         }
-        return weeksAndTransactions;
+        return transactionsForSpecificPeriod;
 
     }
 
-    public static TreeMap<String, TreeMap<String, Integer>> howManyTimesPerWeek(TreeMap<String, String> weeksAndTransactions) {
-        TreeMap<String, TreeMap<String, Integer>> finish = new TreeMap<>();
+    public static TreeMap <String, String> howManyTimesPerPeriode(TreeMap <String, String> weeksAndTransactions) {
+        TreeMap <String, String> finish = new TreeMap<>();
         for (String key : weeksAndTransactions.keySet()) {
             String one = weeksAndTransactions.get(key);
-            List<String> lists = new ArrayList<>(Arrays.asList(one.split(",")));
-            TreeMap<String, Integer> occurenceOfEach = new TreeMap<>();
-            for (String list : lists) {
-                if (occurenceOfEach.containsKey(list)) {
-                    occurenceOfEach.put(list, occurenceOfEach.get(list) + 1);
-                } else {
-                    occurenceOfEach.put(list, 1);
+            String valuePlusOccurence = null;
+            String all = null;
+            if(one.length() == 0) {
+                one = "There is no transactions for this periode";
+                all = one;
+            } else {
+                ArrayList<String> lists = new ArrayList<>(Arrays.asList(one.split(",")));
+                TreeMap<String, Integer> occurenceOfEach = new TreeMap<>();
+                for (String list : lists) {
+                    if (occurenceOfEach.containsKey(list)) {
+                        occurenceOfEach.put(list, occurenceOfEach.get(list) + 1);
+                    } else {
+                        occurenceOfEach.put(list, 1);
+                    }
                 }
+                ArrayList<String> newArray = new ArrayList<>();
+                for (Map.Entry<String, Integer> entry : occurenceOfEach.entrySet()) {
+                    valuePlusOccurence = entry.getKey() + ": " + entry.getValue();
+                    newArray.add(valuePlusOccurence);
+                }
+                all = String.join(", ", newArray);
             }
-//            for (Map.Entry<String, Integer> entry : occurenceOfEach.entrySet()) {
-//                System.out.println(entry.getKey() + ": " + entry.getValue());
-//            }
-            finish.put(key, occurenceOfEach);
+            finish.put(key,  all);
         }
         return finish;
 
     }
 
+    /**
+     * Write Map into File with specific
+     * @param timeAndTransactions
+     * @throws IOException
+     */
+    public static void writeMapIntoFile(TreeMap<String, String> timeAndTransactions) throws FileNotFoundException {
+        String currentTime = LocalDateTime.now().toString().replace(':', '-'); //to make specific name of File
 
+        PrintWriter out = new PrintWriter("logsUpdated" + currentTime + ".txt");
 
+        Set<Map.Entry<String, String>> hashSet=timeAndTransactions.entrySet();
+        for(Map.Entry<String, String>  entry:hashSet ) {
 
-    public static void howManyTimesInString(String myString) {
-
-        List <String> lists = new ArrayList<>(Arrays.asList(myString.split(",")));
-        for(String list : lists) {
-            System.out.println(list);
-        }
-        TreeMap<String, Integer> occurenceOfEach = new TreeMap<>();
-        for (String list : lists) {
-            if(occurenceOfEach.containsKey(list)){
-                occurenceOfEach.put(list, occurenceOfEach.get(list)+1);
-            } else {
-                occurenceOfEach.put(list, 1);
-            }
-        }
-        for(Map.Entry<String, Integer> entry : occurenceOfEach.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+            out.println(entry.getKey() + ":");
+            out.println(entry.getValue());
         }
     }
-
-
-    public static void howManyTimesPerDay(TreeMap<String, String> timeAndTransactions) {
-
-        String one = timeAndTransactions.get("Feb  1 2018");
-        List <String> lists = new ArrayList<>(Arrays.asList(one.split(",")));
-        for(String list : lists) {
-            System.out.println(list);
-        }
-        TreeMap<String, Integer> occurenceOfEach = new TreeMap<>();
-        for (String list : lists) {
-            if(occurenceOfEach.containsKey(list)){
-                occurenceOfEach.put(list, occurenceOfEach.get(list)+1);
-            } else {
-                occurenceOfEach.put(list, 1);
-            }
-        }
-        for(Map.Entry<String, Integer> entry : occurenceOfEach.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-    }
-
-
-
-
-
-
-    public static void date() throws ParseException {
-        String dateAndTime = "Feb  4 2018";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM  d yyyy");
-        LocalDate date = LocalDate.parse(dateAndTime, formatter);
-        LocalDate date2 = date.plusWeeks(2).minusDays(1);
-        System.out.println(date);
-        System.out.println(date2);
-
-    }
-
-
-
 }
