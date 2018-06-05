44adabb73310566d207c80dcf46debead838215f
Анна Чайникова <a.chainikova@gmail.com>
2018-06-05 16:47:24 +0300

    implementing Jira tests

--- src/test/Jira/AppTest.java
+++ src/test/Jira/AppTest.java
@@ -4,18 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static org.openqa.selenium.Keys.ENTER;
+
 
 public class AppTest {
     private static WebDriver driver;
-
+    private String username = "leonid.haidanov";
+    private String password = "kbfybn7323";
+    private String summary = "This is the test ticket";
+    private String errorLoginMessage = "Sorry, your username and password are incorrect - please try again.";
+    private String path;
 
     @DataProvider
     public static Object[][] emailAndPasswordPositive() {
@@ -28,60 +31,57 @@ public class AppTest {
     }
 
     @BeforeTest
-    public void openChrome()  throws InterruptedException {
+    public void openChrome() {
         System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get("http://jira.hillel.it:8080/login.jsp?");
     }
 
+    @AfterTest
+    public void closeChrome() {
+        driver.quit();
+    }
+    @Test
+    public void logInTestPositive() {
+        clearAndFill(By.cssSelector("input[name=os_username]"), username);
+        clearAndFill(By.cssSelector("input[name=os_password]"), password).submit();
+        Assert.assertTrue(driver.findElements(By.cssSelector("a[data-username='" + username + "']")).size()>0);
+    }
 
     @Test
-            //(dataProvider = "emailAndPasswordPositive")
-    public void logInTestPositive(String email, String password) {
-        driver.get("http://jira.hillel.it:8080/login.jsp?");
-        fill(By.cssSelector("input[name=os_username]"), email);
-        fill(By.cssSelector("input[name=os_password]"), password).submit();
-
-        Assert.assertTrue(driver.findElements(By.cssSelector("a[data-username='" + email + "']")).size()>0);
-    }
-
-    @Test (dataProvider = "emailAndPasswordNegative")
-    public void logInTestNegative(String email, String password) {
-        driver.get("http://jira.hillel.it:8080/login.jsp?");
-        fill(By.cssSelector("input[name=os_username]"), email);
-        fill(By.cssSelector("input[name=os_password]"), password).submit();
-
-        Assert.assertFalse(driver.findElements(By.cssSelector("a[data-username='" + email + "']")).size()>0);
+    public void logInTestNegative() {
+        clearAndFill(By.cssSelector("input[name=os_username]"), username);
+        clearAndFill(By.cssSelector("input[name=os_password]"), password+1).submit();
+        Assert.assertEquals (driver.findElement(By.className("aui-message")).getText(), errorLoginMessage);
     }
 
 
-    @Test
-    public void createTicket() throws InterruptedException {
-        logInTestPositive("a.chainikova", "SarbonaRosta" );
-        Thread.sleep(5000);
+    @Test (dependsOnMethods = "logInTestPositive")
+    public void createIssue() throws InterruptedException {
         driver.findElement(By.cssSelector("a[id=create_link]")).click();
         Thread.sleep(5000);
-        driver.findElement(By.cssSelector("input[id=project-field]")).click();
-        fill(By.cssSelector("input[id=project-field]"), "GQR").submit();
+        clearAndFill(By.cssSelector("input[id=project-field]"), "GQR").submit();
         Thread.sleep(5000);
-        //driver.findElement(By.cssSelector("input[id=project-field]")).sendKeys("GQR");
-        //driver.findElement(By.cssSelector("input[id=summary]")).click();
-        fill(By.cssSelector("input[id=summary]"), "This is the test ticket").submit();
-        driver.findElement(By.cssSelector("input[id=create-issue-submit]")).click();
+        clearAndFill(By.cssSelector("input[id=summary]"), summary).submit();
+        Thread.sleep(2000);
+        WebElement successMessage = driver.findElement(By.partialLinkText(summary));
+        Assert.assertTrue(successMessage != null);
+        path =  successMessage.getAttribute("href");
+
+        // <a class="issue-created-key issue-link" data-issue-key="GQR-891" href="/browse/GQR-891">GQR-891 - Test issue</a>
     }
 
+    @Test (dependsOnMethods = {"logInTestPositive", "createIssue"})
+    public void openIssue() {
+        driver.get(path);
+        Assert.assertTrue(driver.getTitle().contains(summary));
+    }
 
-
-
-
-
-
-
-    private static WebElement fill(By selector, String data) {
+    private static WebElement clearAndFill(By selector, String data) {
         WebElement element = driver.findElement(selector);
+        element.clear();
         element.sendKeys(data);
-
         return element;
     }
-
 }
