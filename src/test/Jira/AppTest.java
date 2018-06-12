69d036e10d0b980954482f9302cc7a3ecc11c422
Анна Чайникова <a.chainikova@gmail.com>
2018-06-12 15:30:15 +0300

    implementing upload download methods

--- src/test/Jira/AppTest.java
+++ src/test/Jira/AppTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -14,16 +16,20 @@ import org.testng.annotations.Test;
 
 public class AppTest {
     private static WebDriver driver;
+    private static WebDriverWait wait;
     private String username = "leonid.haidanov";
     private String password = "kbfybn7323";
     private String summary = "This is the test ticket";
     private String errorLoginMessage = "Sorry, your username and password are incorrect - please try again.";
-    private String path;
+    private String ticketPath;
+    private String filePath = "C:\\Users\\anya_\\Downloads\\500.jpeg";
+    private String ticketWithFilePath;
 
     @BeforeTest
     public void openChrome() {
-        System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
         driver.manage().window().maximize();
         driver.get("http://jira.hillel.it:8080/login.jsp?");
     }
@@ -32,6 +38,7 @@ public class AppTest {
     public void closeChrome() {
         driver.quit();
     }
+    
     @Test (priority = 0)
     public void logInTestNegative() {
         clearAndFill(By.cssSelector("input[name=os_username]"), username);
@@ -57,16 +64,49 @@ public class AppTest {
         Thread.sleep(2000);
         WebElement successMessage = driver.findElement(By.partialLinkText(summary));
         Assert.assertTrue(successMessage != null);
-        path =  successMessage.getAttribute("href");
+        ticketPath =  successMessage.getAttribute("href");
 
         // <a class="issue-created-key issue-link" data-issue-key="GQR-891" href="/browse/GQR-891">GQR-891 - Test issue</a>
     }
 
     @Test (priority = 3)
     public void openIssue() {
-        driver.get(path);
+        driver.get(ticketPath);
         Assert.assertTrue(driver.getTitle().contains(summary));
     }
+
+
+    @Test(dependsOnMethods = "logInTestPositive")
+    public void createIssueWithUploadFile() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.findElement(By.cssSelector("a[id=create_link]")).click();
+        Thread.sleep(5000);
+
+        WebElement projectSelect = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[id=project-field]")));
+        clearAndFill(By.cssSelector("input[id=project-field]"), "GQR" +"\n");
+        Thread.sleep(1000);
+        WebElement uploadAttachement = driver.findElement(By.cssSelector("input[type='file']"));
+        uploadAttachement.sendKeys(filePath);
+        WebElement ex = driver.findElement(By.cssSelector("button[class='aui-button aui-button-subtle aui-button-compact']"));
+        Assert.assertTrue((filePath.contains(driver.findElement(By.cssSelector("span[class='upload-progress-bar__file-name']")).getText())) &&
+                ex.isDisplayed());
+        Thread.sleep(1000);
+        clearAndFill(By.cssSelector("input[id=summary]"), summary).submit();
+        WebElement successMessage = wait.until(ExpectedConditions.elementToBeClickable(By.partialLinkText(summary)));
+        ticketWithFilePath =  successMessage.getAttribute("href");
+
+    }
+
+    @Test (dependsOnMethods = {"logInTestPositive", "createIssueWithUploadFile" })
+    public void downloadFile() throws InterruptedException {
+        driver.get(ticketWithFilePath);
+        driver.findElement(By.cssSelector("a[class='attachment-title']")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.cssSelector("a[id=cp-control-panel-download]")).click();
+
+
+    }
+
 
     private static WebElement clearAndFill(By selector, String data) {
         WebElement element = driver.findElement(selector);
@@ -74,4 +114,8 @@ public class AppTest {
         element.sendKeys(data);
         return element;
     }
+
+
+
+
 }
